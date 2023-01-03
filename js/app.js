cecf62941ef5aa9de9be367dfda94d1f0f1f6fbf
rachel_c <rachelclaflin3@gmail.com>
2023-01-03 00:09:02 -0500

    Deleted comment and commented out console.log(randomNumber)

--- js/app.js
+++ js/app.js
@@ -3,13 +3,12 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
-//get random number between 0 - 3
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+    //console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-})
+});
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
